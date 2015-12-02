5c617e6af2bf1227b3e04d343d049e0c85e0a750
Alex Chebotarev <aleksey.chebotarev@gmail.com>
2015-12-02 12:03:58 -0800

    Implement main jQuery function

--- src/jquery_lite.js
+++ src/jquery_lite.js
@@ -1,3 +1,7 @@
-(function () {
-  
-})();
+(function (root) {
+  root.$l = function (selector) {
+    var nodeList = document.querySelectorAll(selector);
+    var nodeArray = Array.prototype.slice.call(nodeList);
+    debugger
+  };
+})(this);
