22d97e25c6521aab02dfd550e2f16770fb4fdc82
Alex Chebotarev <aleksey.chebotarev@gmail.com>
2015-12-02 17:17:54 -0800

    Create DOMNodeCollection class, implement #html

--- src/jquery_lite.js
+++ src/jquery_lite.js
@@ -1,7 +1,30 @@
 (function (root) {
-  root.$l = function (selector) {
-    var nodeList = document.querySelectorAll(selector);
-    var nodeArray = Array.prototype.slice.call(nodeList);
-    debugger
+
+  function DOMNodeCollection (htmlElements) {
+    this.htmlElements = htmlElements;
   };
+
+  DOMNodeCollection.prototype.html = function (str) {
+    if (str) {
+      this.htmlElements.forEach(function (el) {
+        el.innerHTML = str;
+      });
+    } else {
+      return this.htmlElements[0].innerHTML;
+    }
+  };
+
+  root.$l = function (arg) {
+    var nodeArray;
+    
+    if (arg instanceof (HTMLElement)) {
+      nodeArray = [arg];
+    } else {
+      var nodeList = document.querySelectorAll(arg);
+      nodeArray = Array.prototype.slice.call(nodeList);
+    }
+    
+    return new DOMNodeCollection(nodeArray);
+  };
+
 })(this);
