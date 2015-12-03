ba0d56883dd6e00af00a781eee3d2f2cf1d46a2f
Alex Chebotarev <aleksey.chebotarev@gmail.com>
2015-12-03 10:48:41 -0800

    Implement #empty, #append, #each

--- src/jquery_lite.js
+++ src/jquery_lite.js
@@ -4,13 +4,41 @@
     this.htmlElements = htmlElements;
   };
 
+  DOMNodeCollection.prototype.each = function (callback) {
+    this.htmlElements.forEach(function (el) {
+      callback(el);
+    }.bind(this));
+  };
+
   DOMNodeCollection.prototype.html = function (str) {
-    if (str) {
-      this.htmlElements.forEach(function (el) {
+    if (str || str === "") {
+      this.each(function (el) {
         el.innerHTML = str;
       });
     } else {
       return this.htmlElements[0].innerHTML;
+    }
+  };
+
+  DOMNodeCollection.prototype.empty = function () {
+    this.html("");
+  };
+ 
+  DOMNodeCollection.prototype.append = function (obj) {
+    if (obj instanceof (HTMLElement)) {
+      this.each(function (el) {
+        el.appendChild(obj);
+      });
+    } else if (obj instanceof (DOMNodeCollection)) {
+      this.each(function (parentNode) {
+        obj.each(function (childNode) {
+          parentNode.appendChild(childNode);
+        });
+      });
+    } else {
+      this.each(function (el) {
+        el.innerHTML += obj;
+      });
     }
   };
 
